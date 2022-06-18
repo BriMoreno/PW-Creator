cb366b0529400cbabe9cf16bdde7fa18463de5b6
Britania Moreno <69614167+BriMoreno@users.noreply.github.com>
2022-06-17 18:11:21 -0700

    added if loops to concat arrays together

--- Parakeet-copies/script.js
+++ Parakeet-copies/script.js
@@ -1,17 +1,18 @@
-//arrays to chose characters for the password
-var alphabetL = ["abcdefghijklmnopqrstuvwxyz".split("")];
-var alphabetU = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")];
-var validNum = [0,1,2,3,4,5,6,7,8,9];
-var validSymbols = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", "|"];
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate"); //generate is the button
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword); //make the generateBtn function
 
+//arrays to chose characters for the password
+const alphabetL = ["abcdefghijklmnopqrstuvwxyz".split("")];
+const alphabetU = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")];
+const validNum = [0,1,2,3,4,5,6,7,8,9];
+const validSymbols = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", "|"];
+
 function generatePassword() {
 //Prompts asked user to see what they want
-   
+    var charaSet = [];
+    var createPw = "";
     let chara =window.prompt("How many characters would you like in your password?");
     let specChara = window.confirm("Would you like any special characters in your password?(Y/N)");
     let caseU = window.confirm("Would you like uppercase letters?(Y/N)");
@@ -26,6 +27,25 @@ function generatePassword() {
     if (specChara === false && caseU === false && caseL === false && numbers === false) {
         return"Please enter at least 1 type of character to be in your password";
     }
+    if (specChara === true || caseU === true || numbers === true || caseL === true) {
+        if (specChara) {
+            charaSet = charaSet.concat(validSymbols);
+            console.log(charaSet);
+        }
+        if (caseU) {
+            charaSet = charaSet.concat(alphabetU);
+            console.log(charaSet);
+        }
+        if (caseL) {
+            charaSet = charaSet.concat(alphabetL);
+            console.log(charaSet);
+        }
+        if (numbers) {
+            charaSet = charaSet.concat(validNum);
+            console.log(charaSet);
+        }
+
+    }
 };
 
 // Write password to the #password input
@@ -35,5 +55,5 @@ function writePassword() {
    
      passwordText.value = password;
    
-   }
+   };
    
